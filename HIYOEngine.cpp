1da3788fb4c29f1f18207ede5ed32ead398b0547
jys20240601 <jys20240601@gmail.com>
2024-12-20 08:34:32 +0900

    12-20 HomeWork
    Moving Square

--- HIYOEngine.cpp
+++ HIYOEngine.cpp
@@ -1,13 +1,16 @@
 ﻿#include "framework.h"
 #include "HIYOEngine.h"
 
+#include "..\\HIYOEngine_Source\\HIYOApplication.h"
+
+HIYO::APP app;
+
 #define MAX_LOADSTRING 100
 
-HINSTANCE hInst;                                // 현재 인스턴스입니다.
-WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
-WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+HINSTANCE hInst;
+WCHAR szTitle[MAX_LOADSTRING];
+WCHAR szWindowClass[MAX_LOADSTRING];
 
-// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
@@ -21,14 +24,13 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: 여기에 코드를 입력합니다.
+   
 
-    // 전역 문자열을 초기화합니다.
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
     LoadStringW(hInstance, IDC_HIYOENGINE, szWindowClass, MAX_LOADSTRING);
     MyRegisterClass(hInstance);
 
-    // 애플리케이션 초기화를 수행합니다:
+    HWND hWnd;
     if (!InitInstance (hInstance, nCmdShow))
     {
         return FALSE;
@@ -38,7 +40,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     MSG msg;
 
-    // 기본 메시지 루프입니다:
     while (GetMessage(&msg, nullptr, 0, 0))
     {
         if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
@@ -46,18 +47,14 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         }
+
+        app.Run();
     }
 
     return (int) msg.wParam;
 }
 
 
-
-//
-//  함수: MyRegisterClass()
-//
-//  용도: 창 클래스를 등록합니다.
-//
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
     WNDCLASSEXW wcex;
@@ -79,19 +76,9 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     return RegisterClassExW(&wcex);
 }
 
-//
-//   함수: InitInstance(HINSTANCE, int)
-//
-//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
-//
-//   주석:
-//
-//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
-//        주 프로그램 창을 만든 다음 표시합니다.
-//
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+   hInst = hInstance;
 
    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
       CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
@@ -101,30 +88,24 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
       return FALSE;
    }
 
+   app.Initialize(hWnd);
+
    ShowWindow(hWnd, nCmdShow);
    UpdateWindow(hWnd);
 
    return TRUE;
 }
 
-//
-//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  용도: 주 창의 메시지를 처리합니다.
-//
-//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
-//  WM_PAINT    - 주 창을 그립니다.
-//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
-//
-//
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+    
+
     switch (message)
     {
     case WM_COMMAND:
         {
             int wmId = LOWORD(wParam);
-            // 메뉴 선택을 구문 분석합니다:
+
             switch (wmId)
             {
             case IDM_ABOUT:
@@ -142,51 +123,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+
             EndPaint(hWnd, &ps);
         }
         break;
-
-
-    //숙제
-
-    case WM_MOVE:
-    {
-        MessageBoxA(hWnd, "HEY", "MOVE", MB_OK); //움직일시 메시지박스
-    }
-    break;
-
-    case WM_SIZE:
-    {
-        MessageBoxA(hWnd, "BIG", "SIZE", MB_YESNO); //사이징시 YES OR NO 메시지박스
-    }
-    break;
-
-    case WM_LBUTTONDOWN:
-    {
-        if(MessageBoxA(hWnd, "LEFT", "BUTTON", MB_OKCANCEL)==IDOK); //좌측버튼클릭시 메시지박스,OK를 누르면 두번째 메시지박스
-        MessageBoxA(hWnd, "OKOK", "OK", MB_OK);
-    }
-    break;
-
-    case WM_RBUTTONUP:
-    {
-        MessageBoxA(hWnd, "RBUTTON", "UP", MB_ICONWARNING); //클릭하고 떼었을때
-
-    }
-    break;
-
-    case WM_KEYDOWN:
-    {
-        switch (wParam)
-        {
-        case VK_LEFT&& VK_RIGHT&& VK_SPACE:
-        {
-            MessageBoxA(hWnd, "LEFT", "RIGHT", MB_OK);//왼쪽 오른쪽 스페이스바를 동시에 누르면 빕사운드
-        }
-        }
-    }
-    break;
 
     case WM_DESTROY:
         PostQuitMessage(0);
@@ -197,7 +137,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
-// 정보 대화 상자의 메시지 처리기입니다.
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
     UNREFERENCED_PARAMETER(lParam);
